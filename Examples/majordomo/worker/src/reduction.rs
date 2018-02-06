83729294243b9a536090fc99170c9bcb13b8d0bd
Unknown <kjdorow7@gmail.com>
2018-02-05 16:06:13 -0800

    Threads & MD
    In the process of adding Wes's threads with the worker. Having issues decompressing.

---
+++ Examples/majordomo/worker/src/reduction.rs
@@ -0,0 +1,46 @@
+pub fn calc_avg(thread_num: usize, data: Vec<u64>) {
+    let mut sum = 0;
+    for value in data.iter()
+    {
+        sum = sum + value;
+    }
+    let avg = sum / data.len() as u8;
+
+    println!("Thread: {:?} -> Average: {:?}", thread_num, avg);
+}
+
+pub fn calc_sum(thread_num: usize, data: Vec<u64>) {
+    let mut sum = 0;
+    for value in data.iter()
+    {
+        sum = sum + value;
+    }
+
+    println!("Thread: {:?} -> Sum: {:?}", thread_num, sum);
+}
+
+pub fn calc_min(thread_num: usize, data: Vec<u64>) {
+    let mut min = data[0];
+    for value in data.iter()
+    {
+        if value < &min
+        {
+            min = *value;
+        }
+    }
+
+    println!("Thread: {:?} -> Min: {:?}", thread_num, min);
+}
+
+pub fn calc_max(thread_num: usize, data: Vec<u64>) {
+    let mut max = data[0];
+    for value in data.iter()
+    {
+        if value > &max
+        {
+            max = *value;
+        }
+    }
+
+    println!("Thread: {:?} -> Max: {:?}", thread_num, max);
+}
--- Examples/majordomo/worker/src/worker.rs
+++ Examples/majordomo/worker/src/worker.rs
@@ -1,10 +1,14 @@
 extern crate zmq;
 extern crate protobuf;
+extern crate snap;
 
 mod protos;
+mod reduction;
 
 use zmq::{Context, Error};
+use std::thread;
 
+const THREADS: usize = 4;
 
 // client is the subscriber, it will send requests to the client
 fn run_worker(ctx: &mut Context, addr: &str) -> Result<(), Error> {
@@ -15,6 +19,48 @@ fn run_worker(ctx: &mut Context, addr: &str) -> Result<(), Error> {
 
     let send_msg: Vec<u8> = sock.recv_bytes(received_message)?;
     println!("Message received!");
+
+    // extract and perform reduction
+    let mut decompresser = snap::Decoder::new();
+    let mut values: Vec<u8> = decompresser.decompress_vec(&send_msg).unwrap();
+
+    let mut data = message::Message::new();
+    values.
+
+    let mut handles = vec![];
+
+    // print!("Vector: [ ");
+    // for i in values.iter() {
+    //     print!("{:?} ", i);
+    // }
+    // println!("]");
+
+
+
+    for _thread in 0..THREADS {
+        let values_clone = Vec::clone(&values);
+        let handle = thread::spawn(move || {
+            if _thread == 0 {
+                reduction::calc_avg(_thread, values_clone.to_vec());
+            }
+            else if _thread == 1 {
+                reduction::calc_sum(_thread, values_clone.to_vec());
+            }
+            else if _thread == 2 {
+                reduction::calc_min(_thread, values_clone.to_vec());
+            }
+            else {
+                reduction::calc_max(_thread, values_clone.to_vec());
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Reduction complete!");
 
     // send message back to the client
     sock.send(&send_msg, 0)?;
