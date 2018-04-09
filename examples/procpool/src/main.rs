ec4a6ec58aeaca89a67aeb025454b4d6f4af1e9f
Ryan <ryan.joyce@wsu.edu>
2018-04-09 15:58:40 -0700

    Cleaned up, still needs to remove dead processes

--- examples/procpool/src/main.rs
+++ examples/procpool/src/main.rs
@@ -1,33 +1,48 @@
 use std::process::{Command, Stdio};
+// use std::time::{SystemTime};
 
 fn main() {
 
-    let mut procs: Vec<std::process::Child>;
+    let mut procs: Vec<std::process::Child> = Vec::new();
     let mut cmd =
         Command::new("cmd")
-        .args(&["/C", "cargo run --bin hello"])
+        .args(&["/C", "hello.exe"])
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .spawn()
         .unwrap();
+    procs.push(cmd);
 
     let mut cmd2 =
         Command::new("cmd")
-        .args(&["/C", "cargo run --bin worker abc 123"])
+        .args(&["/C", "worker.exe abc 123"])
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .spawn()
         .unwrap();
-
-    procs.append(cmd);
-    // procs.append(cmd2);
+    procs.push(cmd2);
 
     loop {
         // It's streaming here
+        // Broker checks for additional commands here
 
-        let status = cmd.try_wait();
-        let status2 = cmd2.try_wait();
-        println!("Exited with status {:?}", status);
-        println!("Exited with status {:?}", status2);
+        for p in &mut procs {
+            let status = p.try_wait();
+
+            match status {
+                Ok(None) => {
+                    println!("[{:?}] Running...", p.id());
+                }
+                Ok(Some(T)) => {
+                    println!("[{:?}] Status: {:?}", p.id(), status);
+                    // let index = procs.iter().position(|x| *x == p).unwrap();
+                    // procs.remove(index);
+                    // let x = procs.remove_item(&p);
+                }
+                _err => {
+                    println!("Hurr durr, there was an errur");
+                }
+            }
+        }
     }    
 }
